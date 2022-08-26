055fe33267b82418feb8466666a70a80e364bb25
OzanErcan6 <ozanercann@gmail.com>
2022-08-26 18:22:29 +0300

    api connection created

--- src/layouts/Navi.jsx
+++ src/layouts/Navi.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import CartSummary from "./CartSummary";
-import { Button, Container, Dropdown, Menu } from "semantic-ui-react";
+import { Button, Container, Menu } from "semantic-ui-react";
 
 export default function Navi() {
   return (
     <div>
-      <Menu inverted fixed>
+      <Menu inverted fixed="top">
         <Container>
           <Menu.Item name="home" />
           <Menu.Item name="messages" />
 
           <Menu.Menu position="right">
-            <CartSummary/>
+            <CartSummary />
 
             <Menu.Item>
               <Button primary>Sign Up</Button>
